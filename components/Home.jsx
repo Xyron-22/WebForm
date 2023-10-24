43c39aeedcc46ec336240e3db9438249ad6928a5
Xyron-22 <111947997+Xyron-22@users.noreply.github.com>
2023-10-24 09:49:11 +0800

    development stage 3 finalized form

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import toast, {Toaster} from "react-hot-toast";
 import ProductData from '@/data/ProductData';
@@ -11,8 +11,6 @@ import TermsData from '@/data/TermsData';
 import Modal from './Modal';
 
 const HomePage = () => {
-
-  const formData = useRef()
  
   // ORDER DATE -----------------------------------------------------
   //getting the date today
@@ -30,18 +28,12 @@ const HomePage = () => {
   //state for the initial array of outlet names
   const [outletArray, setOutletArray] = useState(Outlet) //do not remove
   //state for outlet name selected
-   
-  // CUSTOMER NAME -----------------------------------------------------
-  //state for customer name
-  const [customerName, setCustomerName] = useState("") //do not remove
 
   // ADDRESS --------------------------------------------------------
   //state for array of address shown based on DSP assigned
   const [arrayOfAddressShown, setArrayOfAddressShown] = useState() //do not remove
   //state for array of address to search against, initial array of address based on dsp
   const [arrayOfAddressToSearchAgainst, setArrayOfAddressToSearchAgainst] = useState() //do not remove
-  //state for address
-  const [address, setAddress] = useState("") //do not remove
 
   // PRODUCTS -------------------------------------------------------------
   //state for single product
@@ -71,7 +63,7 @@ const HomePage = () => {
     deliveryDate: ""
   })
 
-  console.log(form)
+  // console.log(form)
 
   // MODAL TOGGLE ------------------------------------------------------------------
   //state for toggling the modal
@@ -132,7 +124,7 @@ const HomePage = () => {
       && form.remarksFreebiesConcern !== "" 
       && form.deliveryDate !== "") {
         try {
-          await axios.post(process.env.NEXT_PUBLIC_BACKEND_URL + "/api/v1/form", formData.current, {
+          await axios.post(process.env.NEXT_PUBLIC_BACKEND_URL + "/api/v1/form", form, {
             headers: {
               "Content-Type": "application/json"
             }
@@ -153,7 +145,9 @@ const HomePage = () => {
           className: "text-2xl"
         })
       }
-      console.log(formData.current)
+      setDspAssigned("")
+      setArrayOfAddressShown([])
+      setArrayProducts([])
       setForm({
         orderDate: defaultDate,
         dspAssigned: "",
@@ -193,29 +187,33 @@ const HomePage = () => {
  
   return (
     <>
-    <form className='flex-col flex items-center lg:w-[80%] bg-white shadow-2xl' ref={formData} onSubmit={handleSubmit}>
-      <h1 className='m-10 text-xl text-center md:text-3xl'>WESTERN BROTHERS OIL AND LUBRICANTS INC. ORDER FORM</h1>
-      <h3 className='mb-5 text-lg text-center md:text-2xl'>DISTRIBUTOR SALES PERSONNEL ORDER FORM</h3>
-      <hr className='border-2 border-black w-full my-3'/>
-    <label htmlFor='orderDate' className='mt-5 mb-2 text-lg md:text-2xl text-center bg-[#ffcccb] p-1 rounded'>ORDER DATE</label>
-    <input className='shadow bg-whiteSmoke text-xl md:text-2xl' type='date' name='orderDate' value={date.toLocaleDateString("en-CA")} onChange={onSetDate}></input>
-    <hr className='border-2 border-black w-full my-3'/>
-    <h1 className='text-lg md:text-2xl text-center mt-5 mb-2 bg-[#FFD580] p-1 rounded'>DSP ASSIGNED</h1>
+   <form className='flex-col flex items-center lg:w-[80%] bg-white shadow-2xl' onSubmit={handleSubmit}>
+      <h1 className='m-7 md:m-10 text-xl text-center md:text-4xl font-extrabold'>WESTERN BROTHERS OIL AND LUBRICANTS INC. ORDER FORM</h1>
+      <h3 className='mb-5 text-lg text-center md:text-2xl font-bold'>DISTRIBUTOR SALES PERSONNEL ORDER FORM</h3>
+      <hr className='border-[1px] border-black w-[90%] my-3'/>
+    <label htmlFor='orderDate' className='mt-5 mb-2 text-lg md:text-2xl text-center bg-[#ffcccb] p-1 rounded font-semibold'>ORDER DATE</label>
+    <input className='shadow bg-whiteSmoke text-xl md:text-2xl mb-3' type='date' name='orderDate' value={date.toLocaleDateString("en-CA")} onChange={onSetDate}></input>
+    <hr className='border-[1px] border-black w-[90%] my-3'/>
+    <h1 className='text-lg md:text-2xl text-center mt-5 mb-2 bg-[#FFD580] p-1 rounded font-semibold'>DSP ASSIGNED</h1>
     <div className='text-center flex'>
       {DspData.map((dsp, i) => {
         return (
           <div className='m-1 flex flex-col' key={i}>
-          <label htmlFor="dspAssigned" className='text-xl md:text-2xl'>{dsp}</label>
-          <input type='radio' name="dspAssigned" value={dsp} checked={dspAssigned === dsp} onChange={(e) => {
+          <input onClick={(e) => {
             setDspAssigned(e.target.value)
             setForm({...form, dspAssigned: e.target.value})
-          }}></input>
+          }} type='button' value={dsp} className={`text-xl md:text-2xl ${dspAssigned === dsp ? "bg-[#FFD580]" : "bg-transparent"} rounded p-1 mb-1 hover:cursor-pointer`}></input>
+          {/* <input type='radio' name="dspAssigned" value={dsp} checked={dspAssigned === dsp} onChange={(e) => {
+            setDspAssigned(e.target.value)
+            setForm({...form, dspAssigned: e.target.value})
+          }}></input> */}
         </div>
         )
       })}
     </div>
-      <hr className='border-2 border-black w-full my-3'/>
-    <h1 className='text-lg md:text-2xl text-center mt-5 mb-2 bg-[#CBC3E3] p-1 rounded'>OUTLET NAME</h1>
+    <hr className='border-[1px] border-black w-[90%] my-3'/>
+    <h1 className='text-lg md:text-2xl text-center mt-5 mb-2 bg-[#CBC3E3] p-1 rounded font-semibold'>OUTLET NAME</h1>
+    <h1 className='text-lg md:text-2xl text-center mb-2 bg-[#CBC3E3] rounded shadow p-2'>{form?.outlet}</h1>
     <input type='search' onChange={handleSearchOutletName} placeholder='Search Outlet Name' className='text-lg md:text-2xl mt-5 text-center border border-black rounded'></input>
     <div className='w-[95%] flex m-5 flex-wrap text-lg md:text-2xl border border-black rounded-xl h-[20vh] overflow-auto'>
     {outletArray.map((val, i) => {
@@ -225,52 +223,52 @@ const HomePage = () => {
       )
     })}
     </div>
-    <h1 className='text-lg md:text-2xl text-center mb-2 bg-[#CBC3E3] p-1 rounded'>ADDED OUTLET</h1>
-    <h1 className='text-lg md:text-2xl text-center mb-2 bg-[#CBC3E3] rounded shadow p-2'>{form?.outlet}</h1>
-      <hr className='border-2 border-black w-full my-3'/>
-    <h1 className='text-lg md:text-2xl text-center mb-2 bg-[#ADD8E6] p-1 rounded'>CUSTOMER NAME</h1>
-    <h1 className='text-lg md:text-2xl text-center mb-2 bg-[#ADD8E6] rounded shadow p-2'>{customerName}</h1>
-    <input type='text' placeholder='Enter Customer name' name='customerName' val={customerName} className='text-lg md:text-2xl mt-5 text-center border border-black rounded' onChange={(e) => {
-      setCustomerName(e.target.value)
-      setForm({...form, customerName: e.target.value})
-    }}></input>
-      <hr className='border-2 border-black w-full my-3'/>
-    <h1 className='text-lg md:text-2xl text-center mt-5 mb-2 bg-[#C4A484] p-1 rounded'>ADDRESS</h1>
+    <hr className='border-[1px] border-black w-[90%] my-3'/>
+    <h1 className='text-lg md:text-2xl text-center mb-2 bg-[#ADD8E6] p-1 rounded font-semibold'>CUSTOMER NAME</h1>
+    <h1 className='text-lg md:text-2xl text-center mb-2 bg-[#ADD8E6] rounded shadow p-2'>{form.customerName}</h1>
+    <input type='text' placeholder='Enter Customer name' name='customerName' val={form.customerName} className='text-lg md:text-2xl mt-5 text-center border border-black rounded' onChange={(e) => setForm({...form, customerName: e.target.value})}></input>
+    <hr className='border-[1px] border-black w-[90%] my-3'/>
+    <h1 className='text-lg md:text-2xl text-center mt-5 mb-2 bg-[#C4A484] p-1 rounded font-semibold'>ADDRESS</h1>
+    <h1 className='text-lg md:text-2xl text-center mb-2 shadow bg-[#C4A484] p-2 rounded'>{form.address}</h1>
     <input type='search' onChange={handleSearchAddress} placeholder='Search Address' className='text-lg md:text-2xl mt-5 text-center border border-black rounded'></input>
     <div className='w-[95%] flex m-5 flex-wrap text-lg md:text-2xl border border-black rounded-xl h-[20vh] overflow-auto'>
     {arrayOfAddressShown && arrayOfAddressShown.map((address, i) => {
       return (
-        <input type='button' className='h-[25%] m-1 md:m-2 hover:cursor-pointer bg-[#C4A484] rounded p-1 shadow hover:scale-110' name='address' value={address} onClick={(e) => {
-          setAddress(e.target.value)
-          setForm({...form, address: e.target.value})
-        }} key={i}></input>
+        <input type='button' className='h-[25%] m-1 md:m-2 hover:cursor-pointer bg-[#C4A484] rounded p-1 shadow hover:scale-110' name='address' value={address} onClick={(e) => setForm({...form, address: e.target.value})} key={i}></input>
       )
     })}
     </div>
-    <h1 className='text-lg md:text-2xl text-center mb-2 bg-[#C4A484] p-1 rounded'>ADDED ADDRESS</h1>
-    <h1 className='text-lg md:text-2xl text-center mb-2 shadow bg-[#C4A484] p-2 rounded'>{address}</h1>
-      <hr className='border-2 border-black w-full my-3'/>
-    <h1 className='text-lg md:text-2xl text-center mt-5 mb-2 bg-[#D3D3D3] p-1 rounded'>TIN NUMBER AND CONTACT NUMBER</h1>
-    <div className='text-center flex flex-col lg:flex-row'>
+    <hr className='border-[1px] border-black w-[90%] my-3'/>
+    <h1 className='text-lg md:text-2xl text-center mt-5 mb-2 bg-[#D3D3D3] p-1 rounded font-semibold'>TIN NUMBER AND CONTACT NUMBER</h1>
+    <div className='text-center flex flex-col lg:flex-row mb-3'>
       <label htmlFor='TIN' className='text-lg md:text-2xl text-center m-1'>TIN NUMBER:</label>
       <input name='TIN' val={form.tinNumber} type='text' className='text-lg md:text-2xl text-center border border-black rounded' placeholder='Enter TIN' onChange={(e) => setForm({...form, tinNumber: e.target.value})}></input>
       <label htmlFor='Contact' className='text-lg md:text-2xl text-center m-1'>CONTACT NUMBER:</label>
       <input name='Contact' val={form.contactNumber} type='text' className='text-lg md:text-2xl text-center border border-black rounded' placeholder='Enter Contact' onChange={(e) => setForm({...form, contactNumber: e.target.value})}></input>
     </div>
-      <hr className='border-2 border-black w-full my-3'/>
-    <h1 className='text-lg md:text-2xl text-center mb-2 mt-5 bg-[#FFC0CB] p-1 rounded'>TERMS</h1>
+    <hr className='border-[1px] border-black w-[90%] my-3'/>
+    <h1 className='text-lg md:text-2xl text-center mb-2 mt-5 bg-[#FFC0CB] p-1 rounded font-semibold'>TERMS</h1>
     <div className='text-center flex'>
       {TermsData.map((termValue, i) => {
         return (
           <div className='m-1 flex flex-col' key={i}>
-          <label htmlFor={termValue}>{termValue}</label>
-          <input type='radio' name="term" value={termValue} checked={form?.term === termValue} onChange={(e) => setForm({...form, term: e.target.value})}></input>
+          <input type='button' name="term" value={termValue} className={`${form.term === termValue ? "bg-[#FFC0CB]" : "bg-transparent"} m-1 p-1 rounded hover:cursor-pointer text-lg md:text-2xl`} onClick={(e) => setForm({...form, term: e.target.value})}></input>
         </div>
         )
       })}
     </div>
-      <hr className='border-2 border-black w-full my-3'/>
-    <h1 className='text-lg md:text-2xl text-center mb-2 mt-5 bg-lightGreen p-1 rounded'>PRODUCTS</h1>
+    <hr className='border-[1px] border-black w-[90%] my-3'/>
+    <h1 className='text-lg md:text-2xl text-center mb-2 mt-5 bg-lightGreen p-1 rounded font-semibold'>PRODUCTS</h1>
+    <div>
+    {/* <h1 className='text-lg md:text-2xl text-center mb-2 bg-lightGreen p-1 rounded'>ADDED PRODUCTS</h1> */}
+    {arrayProducts.map(({product, quantity, price}, i) => {
+      return (
+        <div className='text-lg md:text-2xl text-center p-2 rounded flex'>
+          <li className='bg-lightGreen p-2 rounded' key={i}>Product: {product} | Quantity: {quantity} | Price: &#x20B1;{price}</li><button className='ml-3 bg-[#FF0000] p-2 rounded' type='button' onClick={(e) => handleRemoveProductOnArray(e, product)}>Remove</button>
+        </div>
+      )
+    })}
+    </div>
     <input type='search' onChange={handleSearchProduct} placeholder='Search Product' className='text-lg md:text-2xl mt-5 text-center border border-black rounded'></input>
     <div className='w-[95%] flex m-5 flex-wrap text-lg md:text-2xl border border-black rounded-xl h-[20vh] overflow-auto'>
     {initialProductArray.map((val, i) => {
@@ -283,25 +281,15 @@ const HomePage = () => {
       )
     })}
     </div>
-    <div className='pb-5'>
-    <h1 className='text-lg md:text-2xl text-center mb-2 bg-lightGreen p-1 rounded'>ADDED PRODUCTS</h1>
-    {arrayProducts.map(({product, quantity, price}, i) => {
-      return (
-        <div className='text-lg md:text-2xl text-center mb-2 shadow bg-lightGreen p-2 rounded'>
-          <li key={i}>Product:{product}, Quantity:{quantity}, Price:&#x20B1;{price} <button type='button' onClick={(e) => handleRemoveProductOnArray(e, product)}>REMOVE</button></li>
-        </div>
-      )
-    })}
-    </div>
-    <hr className='border-2 border-black w-full my-3'/>
-     <h1 className='text-lg md:text-2xl text-center mb-2 mt-5 bg-[#CF9FFF] p-1 rounded'>REMARKS/FREEBIES/CONCERN</h1>
+    <hr className='border-[1px] border-black w-[90%] my-3'/>
+     <h1 className='text-lg md:text-2xl text-center mb-2 mt-5 bg-[#CF9FFF] p-1 rounded font-semibold'>REMARKS/FREEBIES/CONCERN</h1>
     <h1 className='text-lg md:text-2xl text-center mb-2 bg-[#CF9FFF] rounded shadow p-2'>{form?.remarksFreebiesConcern}</h1>
-    <textarea type='text' placeholder='Enter Text' name='remarksFreebiesConcern' className='w-[50%] text-lg md:text-2xl mt-5 text-center border border-black rounded' onChange={(e) => setForm({...form, remarksFreebiesConcern: e.target.value})}></textarea>
-    <hr className='border-2 border-black w-full my-3'/>
-    <h1 className='text-lg md:text-2xl text-center mb-2 mt-5 bg-[#808000] p-1 rounded'>DELIVERY DATE</h1>
+    <textarea type='text' placeholder='Enter Text' name='remarksFreebiesConcern' className='w-[50%] text-lg md:text-2xl mt-5 text-center border border-black rounded mb-3' onChange={(e) => setForm({...form, remarksFreebiesConcern: e.target.value})}></textarea>
+    <hr className='border-[1px] border-black w-[90%] my-3'/>
+    <h1 className='text-lg md:text-2xl text-center mb-2 mt-5 bg-[#808000] p-1 rounded font-semibold'>DELIVERY DATE</h1>
     <input className='shadow bg-whiteSmoke mb-5 text-xl md:text-2xl' type='date' name='deliveryDate' onChange={(e) => setForm({...form, deliveryDate: e.target.value})}></input>
-    <hr className='border-2 border-black w-full mt-3'/>
-    <button type='submit'>submit form</button>
+    <hr className='border-[1px] border-black w-[90%] my-3'/>
+    <button type='submit' className='mb-5 m-2 text-lg md:text-2xl p-2 rounded bg-lightBlue font-semibold'>Submit Form</button>
     {toggle && <Modal setToggle={setToggle} toggle={toggle} product={product} handleProduct={handleProduct} setProduct={setProduct} setArrayProducts={setArrayProducts} arrayProducts={arrayProducts} form={form} setForm={setForm}></Modal>}
     </form>
     <Toaster></Toaster>
