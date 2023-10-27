8ca424e5349191fdfe91abe89b2b603822dc75c7
Xyron-22 <111947997+Xyron-22@users.noreply.github.com>
2023-10-27 20:58:08 +0800

    development stage 4 added reset pass form

---
+++ app/resetpassword/[token]/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ResetPasswordForm from '@/components/ResetPasswordForm'
+
+const ResetPasswordPage = ({params}) => {
+  return (
+   <ResetPasswordForm params={params}></ResetPasswordForm>
+)
+}
+
+export default ResetPasswordPage
---
+++ components/ResetPasswordForm.jsx
@@ -0,0 +1,33 @@
+"use client"
+
+import React, {useState} from 'react'
+import axios from 'axios'
+import { redirect } from 'next/navigation'
+
+const ResetPasswordForm = ({params}) => {
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+    const [errorResponse, setErrorResponse] = useState("")
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const {data} = await axios.patch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/users/resetpassword/${params.token}`, {password, confirmPassword})
+            console.log(data)
+        } catch (error) {   
+            console.log(error)
+            setErrorResponse(error.response?.data?.message)
+        }
+    }
+
+  return (
+    <form onSubmit={handleSubmit}>
+        <h1>{errorResponse}</h1>
+        <input type="password" name='password' onChange={(e) => setPassword(e.target.value)}></input>
+        <input type="password" name='confirmPassword' onChange={(e) => setConfirmPassword(e.target.value)}></input>
+        <button type='submit'>Reset Password</button>
+    </form>
+  )
+}
+
+export default ResetPasswordForm
