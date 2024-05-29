bac2fbea1bdedf9431fcae8201b6659aefa5ce93
Xyron-22 <111947997+Xyron-22@users.noreply.github.com>
2024-05-30 01:50:55 +0800

    fixing export errors in vercel(2)

--- app/auth/layout.js
+++ app/auth/layout.js
@@ -1,8 +1,37 @@
+// import '../../app/globals.css'
+// import Footer from '@/components/Footer'
+// import CurrentFooter from '@/components/Footer(current)'
+// import Header from '@/components/Header'
+// import CurrentHeader from "@/components/Header(current)"
+
+// export const metadata = {
+//   title: 'WesternBrothers',
+//   description: 'Data Management System',
+//   icons: {
+//     icon: "/logo/westernBrothersLogo.jpg"
+//   }
+// }
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en" className='scroll-smooth'>
+//         <body className='min-h-screen min-w-screen'>
+//         <header>
+//           <CurrentHeader></CurrentHeader>
+//         </header>
+//         <div className='h-full min-w-full bg-petron bg-cover bg-center'>
+//         <main className="min-h-[82vh] bg-black bg-opacity-50 relative">
+//           {children}
+//         </main>
+//         </div>
+//         <footer>
+//           <CurrentFooter></CurrentFooter>
+//         </footer>
+//         </body>
+//     </html>
+//   )
+// }
 import '../../app/globals.css'
-import Footer from '@/components/Footer'
-import CurrentFooter from '@/components/Footer(current)'
-import Header from '@/components/Header'
-import CurrentHeader from "@/components/Header(current)"
 
 export const metadata = {
   title: 'WesternBrothers',
@@ -16,17 +45,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className='scroll-smooth'>
         <body className='min-h-screen min-w-screen'>
-        <header>
-          <CurrentHeader></CurrentHeader>
-        </header>
         <div className='h-full min-w-full bg-petron bg-cover bg-center'>
-        <main className="min-h-[82vh] bg-black bg-opacity-50 relative">
+        <main className="min-h-screen bg-black bg-opacity-50 relative">
           {children}
         </main>
         </div>
-        <footer>
-          <CurrentFooter></CurrentFooter>
-        </footer>
         </body>
     </html>
   )
--- app/invoice/layout.js
+++ app/invoice/layout.js
@@ -1,12 +1,14 @@
 // import '../../app/globals.css'
 // import Footer from '@/components/Footer'
+// import CurrentFooter from '@/components/Footer(current)'
 // import Header from '@/components/Header'
+// import CurrentHeader from "@/components/Header(current)"
 
 // export const metadata = {
 //   title: 'WesternBrothers',
 //   description: 'Data Management System',
 //   icons: {
-//     icon: "/logo/oil-drop-logo.png"
+//     icon: "/logo/westernBrothersLogo.jpg"
 //   }
 // }
 
@@ -14,26 +16,22 @@
 //   return (
 //     <html lang="en" className='scroll-smooth'>
 //         <body className='min-h-screen min-w-screen'>
-//         <header className='min-h-[7vh] bg-whiteSmoke flex flex-wrap justify-center items-center'>
-//           <Header></Header>
+//         <header>
+//           <CurrentHeader></CurrentHeader>
 //         </header>
 //         <div className='h-full min-w-full bg-petron bg-cover bg-center'>
 //         <main className="min-h-[86vh] bg-black bg-opacity-50 relative">
 //           {children}
 //         </main>
 //         </div>
-//         <footer className='min-h-[7vh] bg-whiteSmoke flex flex-wrap justify-center items-center'>
-//           <Footer></Footer>
+//         <footer>
+//           <CurrentFooter></CurrentFooter>
 //         </footer>
 //         </body>
 //     </html>
 //   )
 // }
 import '../../app/globals.css'
-import Footer from '@/components/Footer'
-import CurrentFooter from '@/components/Footer(current)'
-import Header from '@/components/Header'
-import CurrentHeader from "@/components/Header(current)"
 
 export const metadata = {
   title: 'WesternBrothers',
@@ -47,17 +45,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className='scroll-smooth'>
         <body className='min-h-screen min-w-screen'>
-        <header>
-          <CurrentHeader></CurrentHeader>
-        </header>
         <div className='h-full min-w-full bg-petron bg-cover bg-center'>
-        <main className="min-h-[86vh] bg-black bg-opacity-50 relative">
+        <main className="min-h-screen bg-black bg-opacity-50 relative">
           {children}
         </main>
         </div>
-        <footer>
-          <CurrentFooter></CurrentFooter>
-        </footer>
         </body>
     </html>
   )
